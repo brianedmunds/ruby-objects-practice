a13a0cc84007a65648538e277342374c8ddd064c
brianedmunds <brian.edmundsjr@outlook.com>
2017-04-24 20:24:50 -0400

    Ruby-objects-practice
    Initial commit.

--- lib/practice.rb
+++ lib/practice.rb
@@ -5,6 +5,11 @@ module MBTA
     # All of the subway lines
     class System
       def initialize
+        subwayLines = {
+          Red: ['South Station', 'Park Street', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
+          Green: ['Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley', 'Hynes","Kenmore'],
+          Orange: ['North Station', 'Haymarket', 'Park Street', 'State', 'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills'],
+        }
       end
 
       def stops_between_stations
